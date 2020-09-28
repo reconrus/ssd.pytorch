0813212a7461f9168249dfeb7947314820a2adda
Danil Ginzburg <danilgin@inbox.ru>
2020-09-28 17:56:57 +0300

    added shipbox class in inmports

--- data/__init__.py
+++ data/__init__.py
@@ -1,7 +1,7 @@
 from .voc0712 import VOCDetection, VOCAnnotationTransform, VOC_CLASSES, VOC_ROOT
 
 from .coco import COCODetection, COCOAnnotationTransform, COCO_CLASSES, COCO_ROOT, get_label_map
-from .ships import ShipsDetection, ShipsTargetTransform, SHIP_CLASSES
+from .ships import ShipsDetection, ShipsTargetTransform, SHIP_CLASSES, ShipBox
 from .config import *
 import torch
 import cv2
